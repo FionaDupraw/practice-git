33f2b376957877ae561b958e115e924a95c8540a
Zach Howarth <zachhowarth@gmail.com>
2019-07-08 13:54:10 -0500

    First Commit

---
+++ Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+//var str = "Hello, playground"
+//
+//var name1 = "Sabyatha"
+//var name2 = "Nailah"
+//
+//print("Hi \(name1) and \(name2)! Glad to see you!")
+
+//Intention: create piece of code that would take each scholar's name annd output each name into a string
+
+//Intention: working with integers and doubles
+//data types: Ints and Doubles
+//
+//var int = 4
+//
+//var double = 5.0
+//
+//print(Double(int) * double)
+
+var a = 12
+var b = 65
+var c = 31
+var d = 98
+
+print(Double(a + b + c + d)/4)
